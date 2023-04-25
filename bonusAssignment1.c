b4a8423b7fb1d4a6cef231d10b136529089a2fc2
mespino873 <73803022+mespino873@users.noreply.github.com>
2023-04-24 22:48:39 -0400

    Create bonusAssignment1.c

---
+++ bonusAssignment1.c
@@ -0,0 +1,68 @@
+// CDA 3103
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#define CACHESIZE 32
+
+void directMapped();
+void twoWayAssociative();
+void fourWayAssociative();
+void fullyAssociative();
+void performanceCalc();
+void randomReplacement();
+
+
+int main()
+{
+	FILE *fp;
+	fp = fopen("traces.txt", "r");
+
+
+
+
+	fclose(fp);
+
+	return 0;
+}
+
+void directMapped()
+{
+
+
+
+}
+
+void twoWayAssociative()
+{
+
+
+}
+
+void fourWayAssociative()
+{
+
+
+
+}
+
+void fullyAssociative()
+{
+
+
+
+}
+
+void performanceCalc()
+{
+
+
+}
+
+void randomReplacement()
+{
+
+	
+}
